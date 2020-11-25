d0cfe53974612c2b16c0147b57ae9a4ca083903a
JakeReadman <jakereadman@gmail.com>
2020-11-25 15:01:42 +0000

    correct component name

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ import UncertaintyApp from './components/UncertaintyApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<UncertaintyApp />, document.getElementById('app'));
--- src/components/UncertaintyApp.js
+++ src/components/UncertaintyApp.js
@@ -5,7 +5,7 @@ import Header from './Header';
 import Options from './Options';
 import OptionModal from './OptionModal';
 
-export default class IndecisionApp extends React.Component {
+export default class UncertaintyApp extends React.Component {
   state = {
     options: [],
     selectedOption: undefined,
@@ -76,12 +76,12 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subTitle={subTitle} />
-        <div className="container">
+        <div className='container'>
           <Action
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
           />
-          <div className="widget">
+          <div className='widget'>
             <Options
               options={this.state.options}
               handleDeleteOptions={this.handleDeleteOptions}
